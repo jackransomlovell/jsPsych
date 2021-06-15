a637decb90564ef6ecbb10aec4e5d4b3a7e0b51f
jackransomlovell <jack.lovell@colorado.edu>
2021-06-14 20:07:27 -0600

    changed video stim

--- demo_task.js
+++ demo_task.js
@@ -1,11 +1,11 @@
-var video = ['examples/video/sample_video.mp4']
+var video = ['duck.mp4']
 var preload = {
   type: 'preload',
   video: video
 } 
 var trial_1 = {
   type: 'collab-filtering',
-  videos: [{stimulus:'examples/video/sample_video.mp4'}],
+  videos: [{stimulus:'duck.mp4'}],
   pause_questions: [{prompt:'whatcha think'},
                     {prompt:'now, tell me how ya really feel'}],
   end_questions: [{prompt:'whatcha think'},
